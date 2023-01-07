cf781c572b1773bdb5183d99735c3ee2ce549ccc
Michel <michaelrocksuperstar@gmail.com>
2023-01-07 16:50:18 +0300

    add first sprite

--- src/main.rs
+++ src/main.rs
@@ -1,4 +1,7 @@
+pub mod pixel_tank_plugin;
+
 use bevy::prelude::*;
+use pixel_tank_plugin::PixelTankPlugin;
 
 fn main() {
     App::new()
@@ -9,5 +12,12 @@ fn main() {
         },
         ..default()
     }))
+    .add_plugin(PixelTankPlugin{})
+    .add_startup_system(setup)
+
     .run();
+}
+
+fn setup(mut commands: Commands) {
+    commands.spawn(Camera2dBundle::default());
 }

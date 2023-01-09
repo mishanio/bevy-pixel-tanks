5b1d3f735cf16da326e4493d2809d9a56ec31dff
Michel <michaelrocksuperstar@gmail.com>
2023-01-09 09:23:52 +0300

    add skeleton for player movement

--- src/game/components.rs
+++ src/game/components.rs
@@ -1,5 +1,36 @@
 
-use bevy::prelude::Component;
+use bevy::prelude::{Component, Vec2};
 
 #[derive(Component)]
 pub struct Despawnable;
+
+
+pub enum MoveDirection {
+    Up,
+    Down,
+    Right,
+    Left,
+}
+
+/// Represents movement of entity
+/// consists of speed value and direction of movement
+#[derive(Component)]
+pub struct Movement {
+    pub speed: f32,
+    pub direction: MoveDirection,
+}
+
+impl Movement {
+    pub fn from(direction: MoveDirection) -> Movement {
+        Movement { speed: 0.0, direction }
+    }
+
+    pub fn to_vec2(&self) -> (f32, f32) {
+        match self.direction {
+            MoveDirection::Up => (0.0, self.speed),
+            MoveDirection::Down => (0.0, -self.speed),
+            MoveDirection::Right => (self.speed, 0.0),
+            MoveDirection::Left => (-self.speed, 0.0),
+        }
+    }
+}
--- src/game/pixel_tank_plugin.rs
+++ src/game/pixel_tank_plugin.rs
@@ -2,14 +2,48 @@ use bevy::prelude::*;
 
 use crate::models::app_state::AppState;
 
-use super::components::Despawnable;
+use super::components::*;
+
+const Z_TANK: f32 = 1.0;
+
+#[derive(Component)]
+struct Acceleration {
+    speed: f32,
+}
+
+#[derive(Component)]
+struct Player;
+
+#[derive(Bundle)]
+struct PlayerTankBundle {
+    player: Player,
+    dispawnable: Despawnable,
+    movement: Movement,
+    acceleration: Acceleration,
+}
+
+impl Default for PlayerTankBundle {
+    fn default() -> Self {
+        Self {
+            player: Player,
+            dispawnable: Despawnable,
+            movement: Movement::from(MoveDirection::Up),
+            acceleration: Acceleration { speed: 1.0 },
+        }
+    }
+}
 
 pub struct PixelTankPlugin;
 
 impl Plugin for PixelTankPlugin {
     fn build(&self, app: &mut App) {
         app.add_system_set(SystemSet::on_enter(AppState::Game).with_system(setup))
-            .add_system_set(SystemSet::on_exit(AppState::Game).with_system(despawn));
+            .add_system_set(SystemSet::on_exit(AppState::Game).with_system(despawn))
+            .add_system_set(
+                SystemSet::on_update(AppState::Game)
+                    .with_system(movement_system)
+                    .with_system(player_tank_control_system),
+            );
     }
 }
 
@@ -19,11 +53,41 @@ fn setup(mut commands: Commands, assets_server: Res<AssetServer>) {
             texture: assets_server.load("tank pixel.png"),
             ..default()
         })
-        .insert(Despawnable);
+        .insert(PlayerTankBundle::default());
 }
 
 fn despawn(mut commands: Commands, q_dispawn: Query<Entity, With<Despawnable>>) {
     for id in q_dispawn.iter() {
         commands.entity(id).despawn_recursive();
+    }
+}
+
+fn player_tank_control_system(
+    keys: Res<Input<KeyCode>>,
+    mut player_tank_query: Query<(&mut Movement, &Acceleration), With<Player>>,
+) {
+    let (mut movement, acceleration) = player_tank_query.single_mut();
+    if keys.pressed(KeyCode::Up) {
+        movement.speed = acceleration.speed;
+        movement.direction = MoveDirection::Up;
+    } else if keys.pressed(KeyCode::Down) {
+        movement.speed = acceleration.speed;
+        movement.direction = MoveDirection::Down;
+    } else if keys.pressed(KeyCode::Right) {
+        movement.speed = acceleration.speed;
+        movement.direction = MoveDirection::Right;
+    } else if keys.pressed(KeyCode::Left) {
+        movement.speed = acceleration.speed;
+        movement.direction = MoveDirection::Left;
+    } else {
+        movement.speed = 0.0;
+    }
+}
+
+fn movement_system(mut q_movement: Query<(&mut Transform, &Movement)>) {
+    for (mut transform, movement) in q_movement.iter_mut() {
+        let (x, y) = movement.to_vec2();
+        transform.translation.x += x;
+        transform.translation.y += y;
     }
 }
